5f5aed4f79222ebe8e7c339b49a5843c0ee4b80d
venqics <venx.rao87@gmail.com>
2021-07-14 21:07:59 +0530

    #tests for job-queue

---
+++ tests/test_jobs.py
@@ -0,0 +1,14 @@
+from flask import url_for
+import pytest
+
+# Functional Tests
+def test_get_rq_admin(client, init_database):
+    response = client.get(url_for('rq_dashboard.overview'))
+    assert response.status_code == 302
+
+def test_get_rq_admin_authed(client, init_database, authenticated_request):
+    authed_user = authenticated_request
+    response = client.get(url_for('rq_dashboard.overview'))
+
+    assert response.status_code == 200
+    assert b'RQ Instances' in response.data
